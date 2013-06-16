4a5c795575e758497c5554220bfaa715c02e0f35
Alexander Sologub <alexsologoub@gmail.com>
2013-06-17 00:40:25 +0300

    Beers scaffolding

---
+++ alcoholic-api/app/controllers/beers_controller.rb
@@ -0,0 +1,28 @@
+class BeersController < ApplicationController
+  def index
+    @beers = Beer.all
+    respond_with @beers
+  end
+
+  def show
+    @beer = Beer.find(params[:id])
+    respond_with @beer
+  end
+
+  def create
+    @beer = Beer.create(params[:beer])
+    respond_with @beer
+  end
+
+  def update
+    @beer = Beer.find(params[:id])
+    @beer.update_attributes(params[:beer])
+    respond_with @beer
+  end
+
+  def destroy
+    @beer = Beer.find(params[:id])
+    @beer.destroy
+    respond_with @beer
+  end
+end
