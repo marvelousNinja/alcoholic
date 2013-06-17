542b583b2800ca76246fc890028d32560f08570b
Alexander Sologub <alexsologoub@gmail.com>
2013-06-17 13:24:37 +0300

    Removed Active Record from UI app

--- alcoholic-ui/config/initializers/secret_token.rb
+++ alcoholic-ui/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-AlcoholicUi::Application.config.secret_token = '336f18e4c446dbc7706588f8a3c91f86486fe85aaab734c63139f4084e41c6d3ed4cc60c8393df4e23717f91788cfbb62621a373432a7476238c799109f73f12'
+AlcoholicUi::Application.config.secret_token = '4cc88c3fdd2bf0d4717e4c510409e2682c5b8e0ba4b46d90415edfeb2d7740c36c32184547649a347864b6644861eb43146a0988c7b5efe08b7a07398f6fcefb'
